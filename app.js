2fb71acf1a732100cb90a8cb13f69cb32a985f8f
kyet-alb <202102017k@utea.edu.pe>
2026-04-27 20:40:44 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let nextButton = document.getElementById("next");
+let prevButton = document.getElementById("prev");
+let backButton = document.getElementById("back");
+let seeMoreButton = document.querySelectorAll(".seeMore");
+let carousel = document.querySelector(".carousel");
+let listHTML = document.querySelector(".carousel .list");
+
+nextButton.onclick = function() {
+    showSlider('next');
+}
+prevButton.onclick = function() {
+    showSlider('prev');
+}
+let unAccepctClick;
+const showSlider = (type) => {
+    nextButton.style.pointerEvents = 'none';
+    prevButton.style.pointerEvents = 'none';
+    carousel.classList.remove('next', 'prev');
+    let items = document.querySelectorAll(".carousel .list .item");
+    if (type === 'next') {
+        listHTML.appendChild(items[0]);
+        carousel.classList.add('next');
+    }else {
+        let positionLast = items.length - 1;
+        listHTML.prepend(items[positionLast]);
+        carousel.classList.add('prev');
+    }
+    
+    clearTimeout(unAccepctClick);
+    unAccepctClick = setTimeout(() => {
+        nextButton.style.pointerEvents = 'auto';
+        prevButton.style.pointerEvents = 'auto';
+    }, 2000);
+}
+
+// seeMoreButton.forEach(button => {
+//     button.onclick = function() {
+//         carousel.classList.add('showDetail');
+//     }
+// })
+backButton.onclick = function() {
+    carousel.classList.remove('showDetail');
+}
